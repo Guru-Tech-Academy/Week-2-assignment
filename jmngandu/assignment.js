f81408a8c56ad9425d3e6a5e6208f5caebef5457
James Ngandu <jamesmngandu@gmail.com>
2023-10-09 00:19:26 +0300

    added question 2

---
+++ jmngandu/assignment.js
@@ -0,0 +1,21 @@
+         //Question 1: Convert to Number Explicitly (15 points)
+
+//(1)  Declare a variable numberStr and assign it a string value
+var numberStr = "42";
+
+//(2)Use explicit type conversion to convert numberStr to a number using the Number() function
+var theNum = Number(numberStr);
+
+//(3) Step 3: Display the converted number in the console
+console.log("Question 1:", theNum);
+
+      //Question 2: Convert to String Explicitly (15 points)
+
+//(1)Declare a variable num and assign it a numerical value, e.g., 2023.
+var num = 2023;
+
+//(2)Use explicit type conversion to convert num to a string using the String() function.
+var theString = String(num);
+
+//(3)Display the converted string in the console.
+console.log("Question 2: ", theString)
